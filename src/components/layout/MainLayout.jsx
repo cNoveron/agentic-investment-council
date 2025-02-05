3581be4817b69766e27dfbd883ce8e92ccf63aef
naikkuma <carlos.escom.ipn@gmail.com>
2025-02-05 10:02:54 -0500

    css: fix: overflowing due to padding

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -2,7 +2,7 @@ import Sidebar from './Sidebar';
 
 export default function MainLayout({ children }) {
   return (
-    <div className="flex min-h-screen w-full surface">
+    <div className="flex min-h-screen surface">
       <Sidebar />
       <main className="flex-1 overflow-hidden">
         {children}
